b50d33fd5105faf3394cc8113af3020b347fd6af
maxlooo <30368159+maxlooo@users.noreply.github.com>
2018-03-27 09:10:26 +0800

    Add files via upload

---
+++ ExtractDataFromFile/ExtractDataFromFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExtractDataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataFile = "C:\\Users\\David\\Documents\\Visual Studio 2017\\Projects\\ExtractDataFromFile\\ExtractDataFromFile\\MessyProgram.txt";
+            string outputFile = "C:\\Users\\David\\Documents\\Visual Studio 2017\\Projects\\ExtractDataFromFile\\ExtractDataFromFile\\Less MessyProgram.txt";
+            string uploadedLineData = "";
+            string[] divideLineData = null;
+
+            FileStream ifs = new FileStream(dataFile, FileMode.Open);
+            StreamReader sr = new StreamReader(ifs);
+            FileStream ofs = new FileStream(outputFile, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(ofs);
+
+            while ((uploadedLineData = sr.ReadLine()) != null)
+            {
+                divideLineData = uploadedLineData.Split(';');
+                for (int i=0; i < divideLineData.Length; ++i)
+                {
+                    sw.WriteLine(divideLineData[i] + ";");
+                }
+                sw.WriteLine(";");
+            }
+
+            sr.Close();
+            ifs.Close();
+            sw.Close();
+            ofs.Close();
+        }
+    }
+}
---
+++ ExtractModifySaveTrainingData/ExtractModifySaveTrainingData/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExtractModifySaveTrainingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program.cs location - C:\Users\David\Documents\Visual Studio 2017\Projects\ExtractModifySaveTrainingData\ExtractModifySaveTrainingData
+
+            string inputFile = "C:\\Users\\David\\Documents\\Visual Studio 2017\\Projects\\ExtractModifySaveTrainingData\\ExtractModifySaveTrainingData\\InputData.txt";
+            string outputFile = "..\\..\\outputData.txt";
+            string inputLineData = "";
+            string outputLineData = "";
+
+            FileStream ifs = new FileStream(inputFile, FileMode.Open);
+            StreamReader sr = new StreamReader(ifs);
+            FileStream ofs = new FileStream(outputFile, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(ofs);
+
+            while ((inputLineData = sr.ReadLine()) != null)
+            {
+                // find start, stop indexes.  get substring.  trim substring.
+                int startIndex = inputLineData.IndexOf("{", StringComparison.Ordinal);
+                int stopIndex = inputLineData.IndexOf("}", StringComparison.Ordinal);
+                Console.WriteLine(startIndex + " - " + stopIndex);
+
+                if (startIndex > 0 && stopIndex > 0)
+                {
+                    outputLineData = inputLineData.Substring(++startIndex, stopIndex - startIndex - 1);
+                    outputLineData = outputLineData.Trim();
+                    sw.WriteLine(outputLineData);
+                }
+            }
+
+            sr.Close();
+            ifs.Close();
+            sw.Close();
+            ofs.Close();
+        }
+    }
+}
